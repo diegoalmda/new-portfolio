e741ea58150e8a210695872e1be1329d6a800c81
Diego Almeida <diegoalmda@gmail.com>
2023-03-22 19:17:29 -0300

    chore: created home content

--- src/app/(public)/components/HomeContentComponent/index.tsx
+++ src/app/(public)/components/HomeContentComponent/index.tsx
@@ -2,10 +2,45 @@
 
 import { PageContainer } from "../../components/PageContainer"
 
+import profilePicture from "../../assets/images/profile-picture.png"
+import profilePurpleTriangle from "../../assets/images/profile-purple-triangle.svg"
+import profileGreenTriangle from "../../assets/images/profile-green-triangle.svg"
+import profilePinkTriangle from "../../assets/images/profile-pink-triangle.svg"
+import littleTriangle from "../../assets/images/little-green-triangle.svg"
+import Image from "next/image"
+
+import styles from "./home.module.scss"
+import { useGlobalContext } from "../../contexts/GlobalApplicationContext"
+import { QuicksandFont } from "../../fonts"
+
 export function HomeContentComponent() {  
+  const { selectedLanguage } = useGlobalContext()
+
   return (
     <PageContainer title="Home">
-      
+      <div className={styles.profileImageContent}>
+        <div className={styles.trianglesContainer}>
+          <Image className={styles.purpleTriangle} src={profilePurpleTriangle} alt="Purple triangle" />
+          <Image className={styles.greenTriangle} src={profileGreenTriangle} alt="Green triangle" />
+          <Image className={styles.pinkTriangle} src={profilePinkTriangle} alt="Pink triangle" />
+        </div>
+        <div className={styles.imageContainer}>
+          <Image src={profilePicture} alt="Diego Almeida photo" />
+        </div>
+      </div>
+      <div className={styles.presentation}>
+        <div className={styles.info}>
+          <h1>{selectedLanguage.home.greetings}</h1>
+          <h2>{selectedLanguage.home.beforeName} <span>Diego de Almeida Cunha</span></h2>
+          <p>{`${selectedLanguage.home.description} `}</p>
+        </div>
+        <Image className={styles.littleTriangle} src={littleTriangle} alt="Green triangle" />
+      </div>
+      <div className={styles.resumeSection}>
+        <a href={selectedLanguage.home.file} title="Diego Almeida's CV" target="_blank" rel="noopener noreferrer">
+          <button className={QuicksandFont.className}>{selectedLanguage.home.doc}</button>
+        </a>
+      </div>
     </PageContainer>
   )
 }
--- src/app/(public)/components/PageContainer/index.tsx
+++ src/app/(public)/components/PageContainer/index.tsx
@@ -2,6 +2,7 @@
 
 import { CaretLeft, CaretRight } from "phosphor-react"
 import { ReactNode } from "react"
+import { QuicksandFont } from "../../fonts"
 
 import styles from "./styles.module.scss"
 
@@ -14,8 +15,8 @@ export function PageContainer({ title, children }: PageContainerProps) {
   return (
     <div className={styles.pageContentContainer}>
       <div className={styles.languageButtons}>
-        <button className={styles.active}>PT</button>
-        <button>EN</button>
+        <button className={`${styles.active} ${QuicksandFont.className}`}>PT</button>
+        <button className={QuicksandFont.className}>EN</button>
       </div>
       {
         title.toLowerCase() !== "home" && <div className={styles.titleContent}><h1>{title}</h1><hr></hr></div>
@@ -29,14 +30,14 @@ export function PageContainer({ title, children }: PageContainerProps) {
           </div>
         } 
         {
-          title.toLowerCase() !== "contact" && title !== "contato" && 
+          title.toLowerCase() !== "contact" && title.toLowerCase() !== "contato" && 
           <div className={styles.arrowRightButton}>
             <button><CaretRight /></button>
             <span>Veja mais</span>
           </div>
         }
       </div>
-      <div>
+      <div className={styles.pageContent}>
         { children }
       </div>
     </div>
